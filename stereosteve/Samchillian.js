d0e162934feee92f346ddf1317af8113b940c3ba
Steve Perkins <stereosteve@gmail.com>
2023-11-01 14:10:26 -0400

    samchillian controls

--- stereosteve/Samchillian.js
+++ stereosteve/Samchillian.js
@@ -2,7 +2,8 @@
 
 	Samchillian 0.1
 
-	A very basic version of Samchillian Tip Tip Tip Cheeepeeeee.
+	A very basic version of Samchillian Tip Tip Tip Cheeepeeeee
+	created by Leon Gruenbaum
 
 	See:
 		https://en.wikipedia.org/wiki/Samchillian
@@ -22,8 +23,64 @@ var NeedsTimingInfo = true;
 // notes above and below will be used to move the cursor a relative amount
 var center = 60;
 
+var outOfRange = 0;
 var upperBound = 108;
 var lowerBound = 21;
+var multiplier = 1;
+var learnMode = false;
+
+var PluginParameters = [
+  {
+    name: "Out of range",
+    type: "menu",
+    valueStrings: ["Wrap", "Constrain", "Drop", "Recenter"],
+    defaultValue: 0,
+    onChange: (v) => (outOfRange = v),
+  },
+  {
+    name: "Low Note",
+    type: "linear",
+    defaultValue: lowerBound,
+    minValue: 0,
+    maxValue: 127,
+    numberOfSteps: 127,
+    onChange: (v) => (lowerBound = v),
+  },
+  {
+    name: "High Note",
+    type: "linear",
+    defaultValue: upperBound,
+    minValue: 0,
+    maxValue: 127,
+    numberOfSteps: 127,
+    onChange: (v) => (upperBound = v),
+  },
+  {
+    name: "Multiplier",
+    type: "linear",
+    defaultValue: 1,
+    minValue: 0,
+    maxValue: 2,
+    numberOfSteps: 20,
+    onChange: (v) => (multiplier = v),
+  },
+  {
+    name: "Recenter",
+    type: "momentary",
+    onChange: () => (cursor = center),
+  },
+  {
+    name: "Learn",
+    type: "momentary",
+    onChange: () => (learnMode = true),
+  },
+];
+
+function ParameterChanged(param, value) {
+  const details = PluginParameters[param];
+  //Trace(`param ${param} ${value}`);
+  details.onChange(value);
+}
 
 // the pitch to move a relative amount on each key press
 var cursor = center;
@@ -31,39 +88,77 @@ var cursor = center;
 var activeNotes = [];
 
 function HandleMIDI(event) {
-	//event.trace();
+  //event.trace();
 
-	if (event instanceof NoteOn) {
-		//silence(event.pitch) ?
+  if (event instanceof NoteOn) {
+    //silence(event.pitch) ?
 
-		event.initialPitch = event.pitch;
-		var diff = event.pitch - center;
-		cursor += diff;
+    if (learnMode) {
+      center = event.pitch;
+      learnMode = false;
+      Trace(`center=${center}`);
+      return;
+    }
 
-		// if outside bounds, wrap around range
-		if (cursor > upperBound) {
-			cursor = lowerBound + (cursor - upperBound);
-		}
-		if (cursor < lowerBound) {
-			cursor = upperBound - (lowerBound - cursor);
-		}
+    event.initialPitch = event.pitch;
+    var diff = (event.pitch - center) * multiplier;
+    cursor += diff;
 
-		event.pitch = cursor;
-		activeNotes.push(event);
-	} else if (event instanceof NoteOff) {
-		silence(event.pitch);
-	}
+    // if outside bounds, wrap around range
+    if (cursor > upperBound) {
+      switch (outOfRange) {
+        case 0:
+          cursor = lowerBound;
+          break;
+        case 1:
+          cursor += diff * -2;
+          break;
+        case 2:
+          cursor = upperBound;
+          return;
+        case 3:
+          cursor = center;
+          break;
+      }
+    }
+    if (cursor < lowerBound) {
+      switch (outOfRange) {
+        case 0:
+          cursor = upperBound;
+          break;
+        case 1:
+          cursor += diff * -2;
+          break;
+        case 2:
+          cursor = lowerBound;
+          return;
+        case 3:
+          cursor = 60;
+          break;
+      }
+    }
 
-	event.send();
+    Trace(
+      `pitch = ${event.pitch} mult = ${multiplier} diff = ${diff} cursor = ${cursor}`
+    );
+
+    event.pitch = cursor;
+    activeNotes.push(event);
+  } else if (event instanceof NoteOff) {
+    silence(event.pitch);
+    return;
+  }
+
+  event.send();
 }
 
 function silence(initialPitch) {
-	const idx = activeNotes.findIndex((n) => n.initialPitch == initialPitch);
-	if (idx > -1) {
-		const note = activeNotes[idx];
-		const off = new NoteOff();
-		off.pitch = note.pitch;
-		off.send();
-		activeNotes.splice(idx, 1);
-	}
+  const idx = activeNotes.findIndex((n) => n.initialPitch == initialPitch);
+  if (idx > -1) {
+    const note = activeNotes[idx];
+    const off = new NoteOff();
+    off.pitch = note.pitch;
+    off.send();
+    activeNotes.splice(idx, 1);
+  }
 }
